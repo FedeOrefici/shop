38c4ab11df46429da3d24b93cec2e17db77e3920
FedeOrefici <federicoaorefici@gmail.com>
2023-08-17 09:28:09 -0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { NavLink } from 'react-router-dom'
 const Container = styled.div`
   width: 100%;
   border: 1px solid black;
+  background-color: #184E77;
 `
 const ContLink = styled.ul`
   list-style: none;
@@ -20,7 +21,7 @@ const Navbar = () => {
   return (
     <Container>
       <ContLink>
-        <NavLink to='/products'>products</NavLink>
+        <NavLink to='/home'>home</NavLink>
         <NavLink to='/yourCoins'>your coins</NavLink>
         <NavLink to='/trade'>trade</NavLink>
       </ContLink>
